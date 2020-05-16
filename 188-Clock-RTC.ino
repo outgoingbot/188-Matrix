0e4bac31c26b89608a71873877931ade2fd41c6c
outgoingbot <outgoingbot@gmail.com>
2020-05-16 11:07:35 -0600

    laptop test 1

--- 188-Clock-RTC.ino
+++ 188-Clock-RTC.ino
@@ -2,7 +2,7 @@
 // left leds x=0
 // top leds y=0
 //rgb 0 = 6 o'clock, increment clockwise
-//change from laptop
+//change from laptop 2
 #include <Adafruit_NeoPixel.h>
 #include "CharTable.h"
 #include <avr/pgmspace.h>
